30f96e2d8ebc0d0025ff055b6ec21941cf2d4c63
Mateus Eduardo <mateussilva184@gmail.com>
2022-10-20 17:17:39 -0300

    Foi instalada a extensão readlineSync, feito a inserção dos GET com os caminhos das operações matematicas

--- Index.js
+++ Index.js
@@ -1,12 +1,10 @@
+const readlineSync = require ("readline-sync")
 const express = require ('express')
 const app = express()
-const bodyparser = require('body-parser')
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.use(bodyparser.urlencoded({extended: true}))
-
-app.get('', (req,res) => {
-    res.send('Olá Mundo')
-})
+const Calc = require('./Controller/Calculadora')
 
 
 app.listen(3000, (err)=> {
@@ -14,5 +12,59 @@ app.listen(3000, (err)=> {
         console.log('400')
     } else {
         console.log('200')
+        
     }
-})
+});
+//app.get('/', (req,res) => {
+    //res.sendFile(__dirname + '/Calculadora.html')
+   // res.status(200).send(`${response}`)
+//});
+
+
+
+
+ function somar (num1, num2){
+     return parseInt(num1) + parseInt(num2)
+ }
+ function multiplicar(num1, num2){
+     return parseInt(num1) * parseInt(num2);
+ }
+ function dividir(num1, num2){
+     return parseInt(num1) / parseInt(num2)
+ }
+ function subtrair(num1, num2){
+     return parseInt(num1) - parseInt(num2)
+ }
+
+
+
+    app.get("/soma", (req, res) => {
+        const n1 = readlineSync.question('digite um numero: ');;
+        const n2 = readlineSync.question('digite um numero: ');;
+        const response = somar(n1,n2)
+    res.send(`<h1>${response}</h1>`)
+   
+})           
+    app.get("/multiplicar", (req,res)=>{
+        const n1 = readlineSync.question('digite um numero: ');
+        const n2 = readlineSync.question('digite um segundo numero: ');
+        const response = ServiceConta.multiplicar(n1,n2)
+        res.send(`<h1>${response}</h1>`)
+    })       
+    app.get("/dividir", (req,res)=>{
+        const n1 = readlineSync.question('digite um numero: ');;
+        const n2 = readlineSync.question('digite um numero: ');;
+        const response = dividir(n1,n2)
+        res.send(`<h1>${response}</h1>`)
+    })    
+    app.get("/subtrair", (req,res)=>{
+        const n1 = readlineSync.question('digite um numero: ');;
+        const n2 = readlineSync.question('digite um numero: ');;
+        const response = subtrair(n1,n2)
+        res.send(`<h1>${response}</h1>`)
+    })
+
+
+
+
+
